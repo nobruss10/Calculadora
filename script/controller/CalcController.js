d6e1254d65d00f537bb1e3421f11651b096f6b3f
Bruno santos <bruno@sequenza.com.br>
2019-01-29 11:14:00 -0200

    método construtor e encapsulamento

--- script/controller/CalcController.js
+++ script/controller/CalcController.js
@@ -1,3 +1,22 @@
 class CalcController {
+    constructor() {
+        this._displayCal = "0";
+        this._dataAtual;
+    }
 
+    get displayCal(){
+        return _displayCal;
+    }
+
+    set displayCal(valor){
+        _displayCal = valor;
+    }
+
+    get dataAtual(){
+        return this._dataAtual;
+    }
+
+    set dataAtual(valor){
+        this._dataAtual = valor;
+    }
 }
