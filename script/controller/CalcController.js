0afc7ac22ef290879b6321ab485996de931c87c9
Bruno santos <bruno@sequenza.com.br>
2019-01-29 14:33:29 -0200

    Criando operações

--- script/controller/CalcController.js
+++ script/controller/CalcController.js
@@ -1,12 +1,14 @@
 class CalcController {
     constructor() {
+
+        this._operation = [];
         this._locale = "pt_BR";
-        this._displayCalEl  = document.querySelector("#display");
+        this._displayCalEl = document.querySelector("#display");
         this._dateEl = document.querySelector("#data");
         this._timeEl = document.querySelector("#hora")
         this._currentDate;
         this.initialize();
-        this. initButtonsEvents();
+        this.initButtonsEvents();
     }
 
     initialize() {
@@ -17,19 +19,83 @@ class CalcController {
 
     }
 
-    addEventListenerAll(element, events, fn){
+    addEventListenerAll(element, events, fn) {
         events.split(' ').forEach(event => {
             element.addEventListener(event, fn, false);
         })
 
     }
 
-    initButtonsEvents(){
+    execBtn(value) {
+        switch (value) {
+            case 'ac':
+                this.clearAll();
+                break;
+            case 'ce':
+                this.clearAll();
+                break;
+            case 'soma':
+                this.clearAll();
+                break;
+            case 'subtracao':
+                this.clearAll();
+                break;
+            case 'divisao':
+                this.clearAll();
+                break;
+            case 'multiplicacao':
+                this.clearAll();
+                break;
+            case 'porcento':
+                this.clearAll();
+                break;
+            case 'igual':
+                this.clearAll();
+                break;
+
+                case '0':
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+                this.addOperation(parseInt(value));
+                break;
+            default:
+                this.setErro();  
+                break;
+
+        }
+    }
+
+    clearAll() {
+        this._operation = [];
+    }
+
+    clearEntry(){
+        this._operation.pop();
+    }
+
+    setErro() {
+        this.displayCal = "Error";
+    }
+
+    addOperation(value){
+        this._operation.push(value);
+        console.log(this._operation);
+    }
+
+    initButtonsEvents() {
         let buttons = document.querySelectorAll("#buttons > g, #parts > g");
 
-        buttons.forEach( (btn, index )=> {
+        buttons.forEach((btn, index) => {
             this.addEventListenerAll(btn, 'click drag', e => {
-                console.log(btn.className.baseVal.replace('btn-', ''));
+                let textBtn = btn.className.baseVal.replace('btn-', '');
+                this.execBtn(textBtn);
             });
             this.addEventListenerAll(btn, 'mouseover mouseup mousedown', e => {
                 btn.style.cursor = "pointer"
@@ -46,35 +112,35 @@ class CalcController {
         this.displayTime = this.currentDate.toLocaleTimeString(this._Locale);
     }
 
-    get displayTime(){
+    get displayTime() {
         return this._timeEl.innerHTML;
     }
 
-    set displayTime(value){
+    set displayTime(value) {
         this._timeEl.innerHTML = value;
     }
 
-    get displayDate(){
+    get displayDate() {
         return this._dateEl.innerHTML;
     }
 
-    set displayDate(value){
+    set displayDate(value) {
         this._dateEl.innerHTML = value;
     }
 
-    get displayCal(){
-        return _displayCal;
+    get displayCal() {
+        return this._displayCalEl.innerHTML;
     }
 
-    set displayCal(value){
-        _displayCal = value;
+    set displayCal(value) {
+        this._displayCalEl.innerHTML = value;
     }
 
-    get currentDate(){
+    get currentDate() {
         return new Date();
     }
 
-    set currentDate(value){
+    set currentDate(value) {
         this._currentDate = value;
     }
 }
