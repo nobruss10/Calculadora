f3a4becb3b13f9994a53c4b89240722a7be4e285
Bruno santos <bruno@sequenza.com.br>
2019-01-29 11:38:39 -0200

    Manipulando o DOM

--- script/controller/CalcController.js
+++ script/controller/CalcController.js
@@ -1,22 +1,33 @@
 class CalcController {
     constructor() {
         this._displayCal = "0";
-        this._dataAtual;
+        this._currentDate;
+        this.initialize();
+    }
+
+    initialize(){
+        let displayCalEl  = document.querySelector("#display");
+        let dateEl = document.querySelector("#data");
+        let timeEl = document.querySelector("#hora")
+
+        displayCalEl.innerHTML =  "12345";
+        dateEl.innerHTML = "29/01/2019";
+        timeEl.innerHTML = "00:00";
     }
 
     get displayCal(){
         return _displayCal;
     }
 
-    set displayCal(valor){
-        _displayCal = valor;
+    set displayCal(value){
+        _displayCal = value;
     }
 
     get dataAtual(){
-        return this._dataAtual;
+        return this._currentDate;
     }
 
-    set dataAtual(valor){
-        this._dataAtual = valor;
+    set dataAtual(value){
+        this._currentDate = value;
     }
 }
