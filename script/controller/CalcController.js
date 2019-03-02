c32ee8ed127b9efcbe4b660259ad347c7656daa6
Bruno santos <bruno@sequenza.com.br>
2019-03-02 17:49:22 -0300

    Eventos de teclado

--- script/controller/CalcController.js
+++ script/controller/CalcController.js
@@ -10,6 +10,7 @@ class CalcController {
         this._currentDate;
         this.initialize();
         this.initButtonsEvents();
+        this.initKeyboard();
     }
 
     initialize() {
@@ -20,6 +21,58 @@ class CalcController {
         }, 1000);
 
         this.setLastNumberToDisplay()
+    }
+
+    initKeyboard() {
+        document.addEventListener('keyup', e =>{
+            console.log(e);
+
+            switch (e.key) {
+                case 'Escape':
+                    this.clearAll();
+                    break;
+                case '':
+                    this.clearEntry();
+                    break;
+                case '+':
+                    this.addOperation('+');
+                    break;
+                case '-':
+                this.addOperation('-');
+                    break;
+                case '/':
+                    this.addOperation('/');
+                    break;
+                case '*':
+                    this.addOperation('*');
+                    break;
+                case '%':
+                    this.addOperation('%');
+                    break;
+                case 'Enter':
+                case '=':
+                    this.calc();
+                    break;
+                case 'ponto':
+                case ',':
+                    this.addDot();
+                    break;
+    
+                case '0':
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+                    this.addOperation(parseInt(e.key));
+                    break;
+            }
+           
+        });
     }
 
     addEventListenerAll(element, events, fn) {
