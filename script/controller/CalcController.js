6a8467bd71294413fbfd4f6482b05573b2859afb
Bruno santos <bruno@sequenza.com.br>
2019-03-03 09:54:11 -0300

    Implementando áudio

--- script/controller/CalcController.js
+++ script/controller/CalcController.js
@@ -1,5 +1,7 @@
 class CalcController {
     constructor() {
+        this._audio = new Audio('click.mp3');
+        this._audioOnOff =  false;
         this._lastOperator = '';
         this._lastNumber = '';
         this._operation = [];
@@ -22,6 +24,23 @@ class CalcController {
 
         this.setLastNumberToDisplay()
         this.pasteFromClipboard();
+
+        document.querySelectorAll('.btn-ac').forEach(btn => {
+            btn.addEventListener('dblclick', e => {
+                this.toogleAudio();
+            })
+        })
+    }
+
+    toogleAudio() {
+        this._audioOnOff = !this._audioOnOff 
+    }
+
+    playAudio() {
+        if (this._audioOnOff) {
+            this._audio.currentTime = 0;
+            this._audio.play();
+        }
     }
 
     copyToClipboard() {
@@ -40,6 +59,8 @@ class CalcController {
 
     initKeyboard() {
         document.addEventListener('keyup', e =>{
+            this.playAudio();
+
             switch (e.key) {
                 case 'Escape':
                     this.clearAll();
@@ -103,6 +124,8 @@ class CalcController {
     }
 
     execBtn(value) {
+        this.playAudio();
+
         switch (value) {
             case 'ac':
                 this.clearAll();
